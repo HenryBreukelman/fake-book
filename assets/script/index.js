0ac09cb38e911a7763e7234f338400b42e53b7fb
Henry <hbreukelman20@gmail.com>
2024-03-27 13:52:14 -0500

    started js

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,3 +1,12 @@
 'use strict';
 
 import * as Utils from './utils.js';
+import {Subscriber} from './Subscriber.js';
+
+const user = Utils.select('.user');
+const text = Utils.select('textarea');
+const image = Utils.select('.image');
+const post = Utils.select('.post');
+
+let posts = []
+
